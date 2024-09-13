ceee44a8b0b6f5f2e690e280774e6a29d23aa4f5
bestiven <bbaqueroalonso@gmail.com>
2024-09-13 12:10:49 -0500

    cambios registrados en el header y footer

--- Code.js
+++ Code.js
@@ -8,3 +8,9 @@ function obtenerDatosHTML(nombre){
  return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 
 }
+
+function obtenerContactos(){
+  let hoja = SpreadsheetApp.openById('1TpuT4NH63g10pDJel7NdV3xg9AhDbovQzNcZD_yCfFM').getActiveSheet();
+  let datos = hoja.getDataRange().getValues();
+  return datos;
+}
