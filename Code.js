84d24203563ca319d49496b9fb0658797c263c95
bestiven <bbaqueroalonso@gmail.com>
2024-09-20 08:47:54 -0500

    Insercion de contactos Api externa Pendiente Notificaciones y edicion de contactos

--- Code.js
+++ Code.js
@@ -27,3 +27,27 @@ function insertarContacto(nombre,apellidos,correo,telf){
 function borrarContacto (numFila){
   HOJA.deleteRow(numFila);
 }
+
+function modificarContacto(numFila,datos){
+  let celdas = HOJA.getRange('A'+numFila+':D'+numFila);
+  celdas.setValues([[datos.nombre,datos.apellidos,datos.correo,datos.telf]]);
+
+}
+//importar contactos desde una Api externa https://randomuser.me/documentation
+function importarContactos(){
+  let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone';
+  let respuesta = UrlFetchApp.fetch(url).getContentText();
+  let datos = JSON.parse(respuesta);
+
+  datos.results.forEach(insertarContactoJSON );
+}
+
+function insertarContactoJSON(contacto){
+HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email,contacto.phone]);
+
+}
+
+
+
+
+
